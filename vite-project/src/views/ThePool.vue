43a195d358e2e014673c25671e39000bdd3f471b
samanthay27 <144460844+samanthay27@users.noreply.github.com>
2024-02-07 10:15:00 -0500

    make more chara

---
+++ vite-project/src/views/ThePool.vue
@@ -0,0 +1,308 @@
+<script setup>
+const thepool = [
+{
+    name: "Noelle",
+    img: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/1a2212b9-af4a-4302-9499-08be7aaa7ff2/de9cbgi-2d6e602b-86fc-4f26-9097-c6a58c83ff6b.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzFhMjIxMmI5LWFmNGEtNDMwMi05NDk5LTA4YmU3YWFhN2ZmMlwvZGU5Y2JnaS0yZDZlNjAyYi04NmZjLTRmMjYtOTA5Ny1jNmE1OGM4M2ZmNmIuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.GIghTJZIZ8LlsvRJ0taSq7nmAskagag5G9rdySu_kr0',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "Ningguang",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "Kaeya",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "Chongyun",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "Sucrose",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "Razor",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "Lisa",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "Fischls",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "Beidou",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "Xingqiu",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "Barbara",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "Xiangling",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "Bennett",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "Amber",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "Qiqi",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "Venti",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "Jean",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "Keqing",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "Mona",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "Diluc",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "Klee",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "Diona",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "Tartaglia",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "Xinyan",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "Zhongli",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "Albedo",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "Ganyu",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+
+  {
+    name: "Xiao",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "Hu Tao",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+
+  {
+    name: "Rosaria",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "Yanfei",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+
+    {
+    name: "Eula",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "Kazuha",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "Kamisato Ayaka",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "Sayu",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "Yoimiya",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "Kujou Sara",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+
+  {
+    name: "Raiden Shogun",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "Sangomiya Kokomi",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "Thoma",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+
+  {
+    name: "Gorou",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "Arataki Itto",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+
+  {
+    name: "",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+  {
+    name: "",
+    img: '',
+    stars: "",
+    standard: true
+  },  
+]
+</script>
+
+<template>
+
+</template>
